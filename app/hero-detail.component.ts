5b3f17dd2d8bcd12072b9c058a0a380de4cb552f
Olle Thalen <olle.thalen@omegapoint.se>
2016-02-25 05:16:02 +0100

    test

--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -1,19 +1,28 @@
-import {Component} from 'angular2/core';
+import {Component, OnInit} from 'angular2/core';
 import {Hero} from './hero';
+import {RouteParams} from 'angular2/router';
+import {HeroService} from './hero.service';
+
 @Component({
   selector: 'my-hero-detail',
-  template: `
-  <div *ngIf="hero">
-    <h2>{{hero.name}} details!</h2>
-    <div><label>id: </label>{{hero.id}}</div>
-    <div>
-      <label>name: </label>
-      <div><input [(ngModel)]="hero.name" placeholder="name"></div>
-    </div>
-  </div>
-  `,
-  inputs: ['hero']
+  templateUrl: 'app/hero-detail.component.html',
+  styleUrls: ['app/hero-detail.component.css']
 })
-export class HeroDetailComponent {
-  public hero: Hero;
+export class HeroDetailComponent implements OnInit {
+  hero: Hero;
+
+  constructor(
+    private _heroService: HeroService,
+    private _routeParams: RouteParams) {
+  }
+
+  ngOnInit() {
+    let id = +this._routeParams.get('id');
+    this._heroService.getHero(id)
+      .then(hero => this.hero = hero);
+  }
+
+  goBack() {
+    window.history.back();
+  }
 }
--- app/hero.service.ts
+++ app/hero.service.ts
@@ -1,10 +1,19 @@
 import {HEROES} from './mock-heroes';
 import {Injectable} from 'angular2/core';
+import {Http} from 'angular2/http';
+//import {Observable} from 'rxjs/Observable';
 import {Hero} from './hero';
+import 'rxjs/add/operator/map'
 
+let apiURL = 'http://localhost/api';
 @Injectable()
 export class HeroService {
+  constructor(private _http:Http) {}
+
   getHeroes() {
+    let result = this._http.get(apiURL)
+            .map(res => res.json());
+    result.subscribe(data => console.log("result: " + JSON.stringify(data)));
     return Promise.resolve(HEROES);
   }
 
@@ -13,4 +22,15 @@ export class HeroService {
       setTimeout(()=>resolve(HEROES), 2000)
     );
   }
+
+  getHero(id: number) {
+    return Promise.resolve(HEROES).then(
+      heroes => heroes.filter(hero => hero.id === id)[0]
+    );
+  }
+
+  handleError(error) {
+        console.error(error);
+        //return Observable.throw(error.json().error || 'Server error');
+    }
 }
